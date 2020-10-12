fafb74475c1aa773c70270edabd24915e59b23be
Varun <31084854+Borumer@users.noreply.github.com>
2020-10-12 16:15:52 -0400

    Solve issue by removing search

--- components/layout.js
+++ components/layout.js
@@ -10,8 +10,6 @@ export default class Layout extends React.Component {
   constructor(props) {
     super(props);
     this.allArticles = getAllArticles();
-    this.searchArticlesKeyType = this.searchArticlesKeyType.bind(this);
-    this.state = {displayedMain: props.children};
   }
 
   render() {
@@ -27,22 +25,9 @@ export default class Layout extends React.Component {
               ))
             }
           </nav>
-          <div>
-            <input type="search" name="q" onKeyUp={this.searchArticlesKeyType} />
-          </div>
         </header>
-        <main>{this.state.displayedMain}</main>
+        <main>{this.props.children}</main>
       </div>
     )
-  }
-
-  searchArticlesKeyType(e) {
-    const query = e.target.value;
-    const filteredArticles = this.allArticles
-      .filter(item => item.title.includes(query) || item.body.includes(query))  
-
-    this.setState({
-      displayedMain: <ArticleList articles={JSON.stringify(filteredArticles)}/>
-    });
   }
 }
