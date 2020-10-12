523b363ca430d1432ccce980d47ec22e1c2c2b9a
Varun <31084854+Borumer@users.noreply.github.com>
2020-10-12 15:55:55 -0400

    Remove style imports

--- components/articlelist.js
+++ components/articlelist.js
@@ -1,4 +1,3 @@
-import styles from './articlelist.module.css';
 import Link from 'next/link';
 
 export default function ArticleList(props) {
--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import React from 'react'
-import layout from './layout.module.css'
 import { getAllArticles } from '../lib/articles'
 import ArticleList from './articlelist'
 
